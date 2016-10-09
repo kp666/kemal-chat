167fd51afa8ef5bcd3befb01a326e90d5946a758
kp <kpisgod@gmail.com>
2016-10-09 19:46:07 +0530

    add websocket chat

--- src/kemal-chat.cr
+++ src/kemal-chat.cr
@@ -1,5 +1,23 @@
 require "./kemal-chat/*"
-
+require "kemal"
 module Kemal::Chat
   # TODO Put your code here
+  get "/" do
+    render "views/index.ecr"
+  end
+
+  SOCKETS = [] of HTTP::WebSocket
+
+  ws "/chat" do |socket|
+    SOCKETS << socket
+    socket.on_message do |message|
+      SOCKETS.each {|socket| socket.send message}
+    end
+
+    socket.on_close do
+      SOCKETS.delete socket
+    end
+  end
 end
+
+Kemal.run
